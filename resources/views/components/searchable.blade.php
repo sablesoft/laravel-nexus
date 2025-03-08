9662ab9aec08d8c33832f88d3a4852d861f6640c
Raman Sobal <v-romansobol@4over.onmicrosoft.com>
2025-03-08 02:04:04 -0300

    updated searchable css

--- resources/views/components/searchable.blade.php
+++ resources/views/components/searchable.blade.php
@@ -8,19 +8,24 @@
 
 <div x-data="searchable('{{ $id }}', '{{ $title }}', @json($keepSelected), @json($allowNew), @json($showSearchableDebug))"
     {{ $attributes->merge(['class' => 'relative']) }}>
-    <button @click.prevent="toggleDropdown" class="w-full px-4 py-2 bg-gray-200 rounded-md shadow-sm text-left cursor-pointer">
+    <button @click.prevent="toggleDropdown"
+            class="cursor-pointer w-full border rounded-lg disabled:shadow-none dark:shadow-none appearance-none text-base sm:text-sm py-2 h-10 leading-[1.375rem] pl-3 pr-3 bg-white dark:bg-white/10 text-zinc-700 placeholder-zinc-400 dark:text-zinc-300 dark:placeholder-zinc-400 shadow-xs border-zinc-200 border-b-zinc-300/80 dark:border-white/10">
         <span x-text="title"></span>
         <template x-if="selectedOption">
-            <button @click.stop.prevent="clearSelection" class="ml-2 text-red-500 hover:text-red-700">&times;</button>
+            <button @click.stop.prevent="clearSelection"
+                    class="cursor-pointer ml-2 text-red-500 hover:text-red-700">&times;</button>
         </template>
     </button>
 
-    <div x-show="dropdownOpen" @click.away="closeDropdown" class="absolute z-10 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg max-h-48 overflow-y-auto">
+    <div x-show="dropdownOpen" @click.away="closeDropdown"
+         class="absolute z-100 mt-1 w-full max-h-48 overflow-y-auto bg-white dark:bg-zinc-700 text-zinc-700 placeholder-zinc-400 dark:text-zinc-300 dark:placeholder-zinc-400 shadow-xs border-zinc-200 border-b-zinc-300 dark:border-zinc-900">
         <!--suppress HtmlFormInputWithoutLabel -->
-        <input type="text" x-model="searchQuery" @keydown.enter.prevent="selectNewOption" placeholder="Search..." class="w-full px-4 py-2 border-b border-gray-300 focus:outline-none">
+        <input type="text" x-model="searchQuery" @keydown.enter.prevent="selectNewOption" placeholder="Search..."
+               class="w-full px-4 py-2 border-b focus:outline-none">
         <ul>
             <template x-for="option in filteredOptions" :key="option.id">
-                <li @click.prevent="selectOption(option)" class="cursor-pointer px-4 py-2 hover:bg-gray-100" x-text="option.name"></li>
+                <li @click.prevent="selectOption(option)"
+                    class="cursor-pointer px-4 py-2 hover:bg-zinc-100 hover:dark:bg-zinc-900" x-text="option.name"></li>
             </template>
         </ul>
     </div>
