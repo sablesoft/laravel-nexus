c43e03a5c96a6f8e99a4044310d04386c86a4a79
Raman Sobal <v-romansobol@4over.onmicrosoft.com>
2025-03-25 21:59:40 -0300

    updated browser-debug config

--- config/browser-debug.php
+++ config/browser-debug.php
@@ -3,13 +3,13 @@
 return [
     'is_enabled' => true,
     'components' => [
-        'echo-presence' => true,
-        'echo-public' => true,
-        'flash' => false,
-        'notification' => false,
-        'searchable' => false,
-        'searchable-multi-select' => false,
-        'searchable-select' => false,
-        'uri' => false,
+        'echo-presence'             => env('DEBUG_COMPONENT_ECHO_PRESENCE', false),
+        'echo-public'               => env('DEBUG_COMPONENT_ECHO_PUBLIC', false),
+        'flash'                     => env('DEBUG_COMPONENT_FLASH', false),
+        'notification'              => env('DEBUG_COMPONENT_NOTIFICATION', false),
+        'searchable'                => env('DEBUG_COMPONENT_SEARCHABLE', false),
+        'searchable-multi-select'   => env('DEBUG_COMPONENT_SEARCHABLE_MULTI_SELECT', false),
+        'searchable-select'         => env('DEBUG_COMPONENT_SEARCHABLE_SELECT', false),
+        'uri'                       => env('DEBUG_COMPONENT_URI', false),
     ]
 ];
